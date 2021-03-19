fd855f869ca1170b8f0d0a48a910263180133043
wll <1909302406@qq.com>
2021-03-19 11:18:46 +0800

    m

--- run_race.sh
+++ run_race.sh
@@ -1,9 +1,9 @@
 python run_race.py \
-  --bert_model bert-large-cased-whole-word-masking \
+  --bert_model bert-base-cased \
   --do_train \
   --do_eval \
-  --train_batch_size 8 \
-  --eval_batch_size 20 \
+  --train_batch_size 4 \
+  --eval_batch_size 4 \
   --num_train_epochs 3.0 \
   --max_seq_length 384 \
   --learning_rate 8e-6 \
