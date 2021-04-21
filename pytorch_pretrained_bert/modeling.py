4c586c8f846f1b43ab3c2ed27c8ef33945643c8a
wll <1909302406@qq.com>
2021-04-21 11:25:05 +0800

    att

--- pytorch_pretrained_bert/modeling.py
+++ pytorch_pretrained_bert/modeling.py
@@ -1608,6 +1608,8 @@ class BertForSequenceClassificationSpanMask(BertPreTrainedModel):
         else:
             return logits
 
+
+
 #改进
 class BertForMultipleChoiceSpanMask2(BertPreTrainedModel):
     """BERT model for multiple choice tasks.
@@ -1667,7 +1669,8 @@ class BertForMultipleChoiceSpanMask2(BertPreTrainedModel):
         self.gamma = nn.Parameter(torch.ones(1))###
         self.classifier = nn.Linear(config.hidden_size, num_choices)##
         self.apply(self.init_bert_weights)
-        #self.ddd=nn.Linear(config.hidden_size*2,config.hidden_size)###改成拼接
+        #self.ddd=nn.Linear(config.hidden_size*2,config.hidden_size)###2:改成拼接
+        self.multihead_attn = nn.MultiheadAttention(768, 12)
     #新增input_span_mask
     def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None, input_span_mask=None):
         input_span_mask = input_span_mask.cuda()###
@@ -1698,11 +1701,17 @@ class BertForMultipleChoiceSpanMask2(BertPreTrainedModel):
 
         span_sequence_output = self.span_layer(sequence_output, extended_span_attention_mask)
         w = F.softmax(self.w)
-        sequence_output = self.gamma * (w[0] * sequence_output + w[1] * span_sequence_output)#原来的做加法
-        # sequence_output=torch.cat([sequence_output,span_sequence_output],2)###改成拼接
-        # sequence_output=self.ddd(sequence_output)###改成拼接
+        #sequence_output = self.gamma * (w[0] * sequence_output + w[1] * span_sequence_output)#原来的做加法
+        # sequence_output=torch.cat([sequence_output,span_sequence_output],2)###2:改成拼接
+        # sequence_output=self.ddd(sequence_output)###2:改成拼接
 
         #交互注意力
+
+        attn_output1, attn_output_weights1 = self.multihead_attn(span_sequence_output, sequence_output,  sequence_output)
+        attn_output2, attn_output_weights2 = self.multihead_attn(span_sequence_output, sequence_output,  span_sequence_output)
+        print('打印')
+        print(attn_output1.shape,attn_output2.shape)
+
 
         pooled_output = self.pooler(sequence_output)
         ###结束变化
